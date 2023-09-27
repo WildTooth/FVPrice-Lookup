e7683836839570ebaa93d49e5d8e738a5bf798bb
WildTooth <champenv@outlook.dk>
2023-09-27 21:29:10 +0200

    Implemented cross-version compatability

--- core/src/main/kotlin/com/github/wildtooth/fvtp/listener/ItemStackTooltipListener.kt
+++ core/src/main/kotlin/com/github/wildtooth/fvtp/listener/ItemStackTooltipListener.kt
@@ -3,7 +3,7 @@ package com.github.wildtooth.fvtp.listener
 import com.github.wildtooth.fvtp.item.SkullItem
 import com.github.wildtooth.fvtp.storage.InformationStorage
 import net.labymod.api.client.component.Component
-import net.labymod.api.client.component.TextComponent
+import net.labymod.api.client.component.serializer.plain.PlainTextComponentSerializer
 import net.labymod.api.client.world.item.ItemStack
 import net.labymod.api.event.Subscribe
 import net.labymod.api.event.client.world.ItemStackTooltipEvent
@@ -13,12 +13,19 @@ class ItemStackTooltipListener {
   fun onItemStackTooltipEvent(event: ItemStackTooltipEvent) {
     val eventItemStack: ItemStack = event.itemStack()
     val storage: InformationStorage = InformationStorage.instance!!
-    if (storage.get((eventItemStack.displayName as TextComponent).text.lowercase()) == null) {
+    if (storage.get(
+        PlainTextComponentSerializer.plainText().serialize(eventItemStack.displayName).lowercase()
+    ) == null) {
       return
     }
-    val fvItem = storage.get((eventItemStack.displayName as TextComponent).text.lowercase())
+    val fvItem = storage.get(
+      PlainTextComponentSerializer.plainText().serialize(eventItemStack.displayName).lowercase()
+    )
     if (fvItem is SkullItem) {
-      if (!event.itemStack().identifier.path.equals("skull")) {
+      if (!event.itemStack().identifier.path.equals("skull") and
+        !event.itemStack().identifier.path.contains("_head") and
+        !event.itemStack().identifier.path.contains("_skull")
+        ) {
         return
       }
       if (fvItem.isSellersPrice()) {
