c70f64942b76b9d7cb1294e9c6dd4ea71b173e67
WildTooth <champenV@outlook.dk>
2023-09-29 14:13:10 +0200

    Added section headers and a debug setting

--- core/src/main/kotlin/com/github/wildtooth/fvtp/FreakyVilleAddonConfiguration.kt
+++ core/src/main/kotlin/com/github/wildtooth/fvtp/FreakyVilleAddonConfiguration.kt
@@ -4,13 +4,21 @@ import net.labymod.api.addon.AddonConfig
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting
 import net.labymod.api.configuration.loader.annotation.ConfigName
 import net.labymod.api.configuration.loader.property.ConfigProperty
+import net.labymod.api.configuration.settings.annotation.SettingSection
 
 
 @ConfigName("settings")
 class FreakyVilleAddonConfiguration : AddonConfig() {
   @SwitchSetting
+  @SettingSection("general")
   private val enabled = ConfigProperty(true)
+  @SwitchSetting
+  @SettingSection("miscellaneous")
+  private val debug = ConfigProperty(false)
   override fun enabled(): ConfigProperty<Boolean> {
     return enabled
+  }
+  fun debug(): ConfigProperty<Boolean> {
+    return debug
   }
 }
