0ec70b6ad7084365bdee0ea4d4e35cb8f1f348fc
WildTooth <champenv@outlook.dk>
2024-01-27 00:21:56 +0100

    Corrected LabyGradle version and removed trailing comma in en_us.json

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "FVPrice-Lookup"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.30"
+    val labyGradlePluginVersion = "0.3.44"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
